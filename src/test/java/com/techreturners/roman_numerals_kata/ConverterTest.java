6199aec210bb5516920ed8614c3db40f5cc91a01
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-03 19:46:46 +0100

    Refractor unit tests

--- src/test/java/com/techreturners/roman_numerals_kata/ConverterTest.java
+++ src/test/java/com/techreturners/roman_numerals_kata/ConverterTest.java
@@ -2,24 +2,27 @@ package com.techreturners.roman_numerals_kata;
 
 import com.techreturners.roman_numerals_kata.app.Converter;
 import com.techreturners.roman_numerals_kata.model.Numeral;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConverterTest {
 
+    Converter c;
+
+    @BeforeEach
+    public void setUp() {
+        c = new Converter();
+    }
 
     @Test
-    public void test0(){
-        Converter c = new Converter();
-
+    public void test0() {
         assertEquals(null, c.convertNumberToNumeral(0));
     }
 
     @Test
-    public void test1(){
-        Converter c = new Converter();
-
+    public void test1() {
         assertEquals(Numeral.I, c.convertNumberToNumeral(1));
     }
 
